ad51078cfa79ff8c6a8641bb42ff3d8c05d93328
Prasad Basutkar <32088778+prasadbasutkar@users.noreply.github.com>
2023-07-05 11:29:31 +0100

    Omdi 53 : changes for dbgap (#2)
    * OMDI-53 : update dbgap based on latest xml data
    
    * OMDI-53 : update dbgap based on latest xml data
    
    * OMDI-53 : update dbgap based on latest xml data
    
    ---------
    
    Co-authored-by: prasadb <prasadb@ebi.ac.uk>

--- ddi-index-pipeline/src/main/java/uk/ac/ebi/ddi/retriever/providers/DbGapFileUrlRetriever.java
+++ ddi-index-pipeline/src/main/java/uk/ac/ebi/ddi/retriever/providers/DbGapFileUrlRetriever.java
@@ -1,6 +1,9 @@
 package uk.ac.ebi.ddi.retriever.providers;
 
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPConnectionClosedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.ac.ebi.ddi.ddidomaindb.database.DB;
 import uk.ac.ebi.ddi.extservices.net.FtpUtils;
 import uk.ac.ebi.ddi.extservices.net.UriUtils;
@@ -16,6 +19,7 @@ import java.util.Set;
 
 public class DbGapFileUrlRetriever extends DatasetFileUrlRetriever {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DbGapFileUrlRetriever.class);
     private static final String FTP_DBGAP = "ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies";
 
     public DbGapFileUrlRetriever(IDatasetFileUrlRetriever datasetDownloadingRetriever) {
@@ -25,17 +29,21 @@ public class DbGapFileUrlRetriever extends DatasetFileUrlRetriever {
     @Override
     public Set<String> getAllDatasetFiles(String accession, String database) throws IOException {
         Set<String> result = new HashSet<>();
-        String url = String.format("%s/%s", FTP_DBGAP, accession);
+        String url = String.format("%s/%s%s", FTP_DBGAP, accession, "?disconnect=true");
         URI uri = UriUtils.toUri(url);
         FTPClient ftpClient = createFtpClient();
         try {
+            ftpClient.setRemoteVerificationEnabled(false);
             ftpClient.connect(uri.getHost());
             ftpClient.login("anonymous", "anonymous");
             FtpUtils.getListFiles(ftpClient, uri.getPath()).stream()
                     .map(x -> String.format("ftp://%s%s", uri.getHost(), x))
                     .forEach(result::add);
+        } catch (FTPConnectionClosedException e) {
+            LOGGER.error("Error while retrieving dataset files for {} {}",accession, e.getMessage() );
         } finally {
             if (ftpClient.isConnected()) {
+                ftpClient.quit();
                 ftpClient.disconnect();
             }
         }
---
+++ ddi-index-pipeline/src/test/java/uk/ac/ebi/ddi/dbgap/DbgapServiceTest.java
@@ -0,0 +1,29 @@
+package uk.ac.ebi.ddi.dbgap;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.File;
+import java.io.IOException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/applicationTestContext.xml"})
+public class DbgapServiceTest {
+
+    @Autowired
+    DbgapService dbgapService;
+
+    @Test
+    public void testDbgap() throws IOException {
+        String resourceName = "GapExchange_phs001826.v1.p1.xml";
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(resourceName).getFile());
+        dbgapService.saveEntries(file.getAbsolutePath());
+        Assert.assertNotNull(file.getAbsolutePath());
+    }
+}
